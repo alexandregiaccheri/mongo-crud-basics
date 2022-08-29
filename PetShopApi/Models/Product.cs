5f11476e142f97650067ca0a0021297403b1b0c2
alexandregiaccheri <alexandregiaccheri@outlook.com>
2022-08-28 23:06:38 -0300

    feat: product model

---
+++ PetShopApi/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PetShopApi.Models
+{
+    public class Product
+    {
+        [BsonRequired]
+        public DateTime CreationDate { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRequired]
+        public string ImgUrl { get; set; } = null!;
+
+        [BsonRequired]
+        public DateTime LastUpdated { get; set; }
+
+        [BsonRequired]
+        public string Manufacturer { get; set; } = null!;
+        
+        [BsonRequired]
+        public string ProductDescription { get; set; } = null!;
+        
+        [BsonRequired]
+        public int ProductPrice { get; set; }
+
+        [BsonRequired]
+        public string ProductTitle { get; set; } = null!;
+
+        [BsonRequired]
+        public int Stock { get; set; }
+    }
+}
